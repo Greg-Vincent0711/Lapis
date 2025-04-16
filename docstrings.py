1e79db6e614eb5a285896c019cc63f71cfdc3d0e
Gregory Vincent Jr <gregvjrr@gmail.com>
2025-04-16 14:32:01 -0400

    Updated docstrings

--- docstrings.py
+++ docstrings.py
@@ -1,34 +1,36 @@
 saveDocString = '''
     !save location_name "X Y Z"
-    Use double quotes for any spaces.
-    !save Nether "34 -20 221"
-    !save "Nether Hub" "32 121 120"
+        Use double quotes for any spaces.
+        !save Nether "34 -20 221"
+        !save "Nether Hub" "32 121 120"
     ''' 
 getDocString = '''
     !get location_name.
-    If you save a name as 'Nether', retrive it using !get 'Nether'
-    If a name is saved as Nether, retrieve it using !get Nether
-    !get "Villager Center" - use double quotes for spaces
+        If you save a name as 'Nether', retrive it using !get 'Nether'
+        If a name is saved as Nether, retrieve it using !get Nether
+        !get "Villager Center" - use double quotes for spaces
     '''
 updateDocString = '''
-    !update location_name"
+    !update location_name
+        Use the exact name of the location.
+        If it's 'Nether', call !update 'Nether'
 '''
 deleteDocString = '''
     !delete location_name
-    This will automatically delete any corresponding image for a location.
-    If you want to delete specifically the image, call !deleteImg location_name
+        This will automatically delete any corresponding image for a location.
+        If you want to delete specifically the image, call !deleteImg location_name
 '''
 listDocString = '''
     See all locations you have saved.
 '''
 saveImgDocString='''
     Store an image for a saved place.
-    Send an attachment when using this function
-    !saveImage location_name 
+        Send an attachment when using this function(copy and paste an image into the message bar before sending)
+        !saveImage location_name 
 '''
 deleteImgDocString = '''
     Delete an image stored for a location.
-    If you'd like to replace an image instead, use saveImage with the same location name. 
+        If you'd like to replace an image instead, use saveImage with the same location name. 
 '''
 
 helpDocString = '''
