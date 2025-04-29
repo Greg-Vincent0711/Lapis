86c86971d9889c120e46cce7a4d769042e59ab90
Gregory Vincent Jr <gregvjrr@gmail.com>
2025-04-28 21:01:11 -0400

    Added comments to explain findNearestStructure.

--- SeedInfoFns/seedInfoFns.c
+++ SeedInfoFns/seedInfoFns.c
@@ -14,10 +14,10 @@ void setUpBiomeGenerator(){
     applySeed(&biomeGenerator, DIM_OVERWORLD, TEST_SEED);
 }
 /**
- * corresponds to !nearest command for Lapis
+ * corresponds to !nearest <biome> coords range for Lapis
  * for now, the seed is going to have to be hardcoded
  * returns 2d coords for biome (x and z)
- * Not super accurate, need to test
+ * Relatively accurate for Bedrock and Java
 */
 Pos nearestBiome(char *biome, int xCoord, int yCoord, int zCoord, int range, enum BiomeID bID){ 
     setUpBiomeGenerator();
@@ -32,19 +32,35 @@ Pos nearestBiome(char *biome, int xCoord, int yCoord, int zCoord, int range, enu
                         xCoord, yCoord, zCoord, range, validBiome, 0, &rand, NULL); 
 }
 
+/**
+ * corresponds to !nearest <structure_id> coords range for Lapis
+ * Bedrock is not 100% accurate when using this function, diff structure gen than Java
+*/
 Pos findNearestStructure(enum StructureType sType, int blockX, int blockZ, int maxRadius) {
     Pos nearest = {0};
+    // initialize first
     long minDistSq = LONG_MAX;
+    long maxBlockDistSq = LONG_MAX;
+
+    /**
+     * region coordinates represent 512 blocks at a time
+     * right-shifting by 9 switches coordinates to region coords
+    */
     int startRegionX = blockX >> 9;
     int startRegionZ = blockZ >> 9;
     int maxRegionRadius;
-    long maxBlockDistSq = LONG_MAX;
+    
 
-    // block based radius
+    /**
+     * if the maxRadius is 10 or less, treat it like a region radius (maxRadius << 9)
+     * Past 10, it doesn't seem too practical, since most structures a player probably wants to find
+     * is within 5120 blocks. 
+     * 1 <= maxRadius <= 10 == region
+     * maxRadius >= 10, treat it as a block radius
+    */
     if (maxRadius >= 11) {
         maxBlockDistSq = (long)maxRadius * (long)maxRadius;
         maxRegionRadius = (maxRadius >> 9) + 2;
-    // region-based radius 
     } else {
         maxRegionRadius = maxRadius;
     }
@@ -52,19 +68,22 @@ Pos findNearestStructure(enum StructureType sType, int blockX, int blockZ, int m
     for (int currentRadius = 0; currentRadius <= maxRegionRadius; currentRadius++) {
         for (int dx = -currentRadius; dx <= currentRadius; dx++) {
             for (int dz = -currentRadius; dz <= currentRadius; dz++) {
-                // Only check edges of the current ring
-                if (abs(dx) != currentRadius && abs(dz) != currentRadius)
+                if (abs(dx) != currentRadius && abs(dz) != currentRadius){
                     continue;
-
+                }
                 int regionX = startRegionX + dx;
                 int regionZ = startRegionZ + dz;
 
+
                 Pos structurePos;
+                // getStructurePos updates the Pos passed to it in-place
                 if (getStructurePos(sType, biomeGenerator.mc, biomeGenerator.seed, regionX, regionZ, &structurePos)) {
+                    // after a block position is found, check if it's viable
                     if (!isViableStructurePos(sType, &biomeGenerator, structurePos.x, structurePos.z, 0)) {
                         continue;
                     }
 
+                    // compare the distance for each X,Z coordinate with the current min
                     long distanceX = structurePos.x - blockX;
                     long distanceZ = structurePos.z - blockZ;
                     long distanceSq = distanceX * distanceX + distanceZ * distanceZ;
@@ -73,6 +92,7 @@ Pos findNearestStructure(enum StructureType sType, int blockX, int blockZ, int m
                         // only save locations within the maxBlock distance if using block radius
                         if (maxRadius >= 11 && distanceSq > maxBlockDistSq)
                             continue;
+                        // otherwise, update the nearest Pos 
                         minDistSq = distanceSq;
                         nearest = structurePos;
                     }
@@ -80,9 +100,12 @@ Pos findNearestStructure(enum StructureType sType, int blockX, int blockZ, int m
             }
         }
 
+
+    // check if a closer structure exists in any future regions
         if (minDistSq != LONG_MAX) {
-            // farthest possible distance from the current radius is 512 blocks
+            // check within the next max possible radius
             long maxPossibleDist = ((long)currentRadius + 1) * 512L;
+            // for distance, use squared over sqrt for performance
             maxPossibleDist *= maxPossibleDist;
             if (minDistSq <= maxPossibleDist) {
                 break;
@@ -102,8 +125,6 @@ Pos findNearestStructure(enum StructureType sType, int blockX, int blockZ, int m
 
 Pos nearestStructure(enum StructureType sType, int blockX, int blockZ, int maxRadius){
     setUpBiomeGenerator();
-    // int structureCheck = isViableStructurePos(sType, &biomeGenerator, blockX << 4, blockZ << 4, 0);
-    // printf("%d", structureCheck);
     Pos structureCoords = findNearestStructure(sType, blockX, blockZ, maxRadius);
     if (structureCoords.x == -1 && structureCoords.z == -1) {
         printf("No structure found within range.\n");
@@ -112,16 +133,12 @@ Pos nearestStructure(enum StructureType sType, int blockX, int blockZ, int maxRa
 }
 
 /**
- * !nearestBiome
- * !nearest
- * !random
+ * !nearest - Biome & Structure
  * !spawn_near
- * !locate
 */
 int main(int argc, char *argv[]){
     char *command = argv[1];
     char *argument = argv[2];
-    printf("%s %s \n", command, argument);
     if(strcmp(command, "nearest") == 0){
         enum BiomeID bID = get_biome_id(argument);
         enum StructureType sType = get_structure_id(argument);
@@ -138,7 +155,6 @@ int main(int argc, char *argv[]){
             int zCoord = atoi(argv[4]);
             int range = atoi(argv[5]);
             printf("%d %d %d\n", xCoord, zCoord, range);
-            // Bedrock is 100% accurate when using this function, diff structure gen than Java
             Pos structureCoords = nearestStructure(sType, xCoord, zCoord, range);
             printf("Nearest %s: %d, %d", argument, structureCoords.x, structureCoords.z);
         } else {
@@ -147,12 +163,8 @@ int main(int argc, char *argv[]){
         
     } else if(strcmp(command, "spawn_near")){
 
-    } else if(strcmp(command, "locate")){
-
     } else{
-
+        printf("Invalid command. Check the list of all possible commands and try again.");
     }
     return 0;
 }
-
-// ./seedInfoFns 'nearestBiome' plains 0 0 0 1024
