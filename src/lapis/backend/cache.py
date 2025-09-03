ead4aa4cebd974cc0f21659863939780af299df6
Gregory Vincent Jr <gregvjrr@gmail.com>
2025-09-02 20:09:17 -0400

    Updated cache.py

--- src/lapis/backend/cache.py
+++ src/lapis/backend/cache.py
@@ -5,17 +5,22 @@ Reduces # of GET requests to DynamoDB if user calls a lot of seedInfoFns - all o
 import sqlite3
 import os
 
-conn = sqlite3.connect(os.getenv("SEED_CACHE"))
-cursor = conn.cursor()
 
-cursor.execute(f"""
-CREATE TABLE IF NOT EXISTS {os.getenv("CACHE_TABLE")} (
-    user_id TEXT PRIMARY KEY,
-    seed TEXT
-)""")
-conn.commit()
+def get_connection_and_cursor():
+    conn = sqlite3.connect(os.getenv("SEED_CACHE"))
+    cursor = conn.cursor()
+    cursor.execute(f"""
+    CREATE TABLE IF NOT EXISTS {os.getenv("CACHE_TABLE")} (
+        user_id TEXT PRIMARY KEY,
+        seed TEXT
+    )""")
+    conn.commit()
+    return conn, cursor
+
+
 
 def cache_user_seed(user_id, seed):
+    conn, cursor = get_connection_and_cursor()
     current = get_cached_seed(user_id)
     if current != seed:
         cursor.execute(f"""
@@ -25,11 +30,13 @@ def cache_user_seed(user_id, seed):
         conn.commit()
 
 def get_cached_seed(user_id):
+    conn, cursor = get_connection_and_cursor()
     cursor.execute(f"SELECT seed FROM {os.getenv('CACHE_TABLE')} WHERE user_id = ?", (str(user_id),))
     row = cursor.fetchone()
     return row[0] if row else None
 
 # may not be used until project becomes more complex
 def invalidate_user_cache(user_id):
+    conn, cursor = get_connection_and_cursor()
     cursor.execute(f"DELETE FROM {os.getenv('CACHE_TABLE')} WHERE user_id = ?", (str(user_id),))
     conn.commit()
