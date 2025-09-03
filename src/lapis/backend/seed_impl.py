962a3e819e578c3ac64c4bc3f2674113686008cf
Gregory Vincent Jr <gregvjrr@gmail.com>
2025-09-02 21:20:47 -0400

    Updated seed_impl, keeping it like this for now

--- src/lapis/backend/seed_impl.py
+++ src/lapis/backend/seed_impl.py
@@ -43,10 +43,9 @@ async def nearest_impl(interaction: discord.Interaction, feature: str, x_coord: 
 
 async def spawn_near_impl(interaction: discord.Interaction, numseeds: str, range: str, biome: Optional[str] = "None", structure: Optional[str] = "None"):
     range = int(range)
-    env = retrieveEnv()
     await interaction.response.defer()
     await interaction.followup.send(f"Finding {numseeds} seeds with: a {biome} spawn, and a {structure} within {range} blocks...")
-    arguments = [env, numseeds, format_feature(biome), format_feature(structure), range]
+    arguments = ["spawn_near", numseeds, format_feature(biome), format_feature(structure), range]
     retrievedSeeds = connectToInputHandler(interaction.user.id, arguments)
     if not "error" in retrievedSeeds[0]:
         formattedRes = ""
